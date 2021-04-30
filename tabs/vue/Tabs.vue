35603bca1a07dc13f959d9d97e41543ab3968e03
osvaldom <mekymaria@gmail.com>
2021-05-01 00:45:24 +0200

    Add initial version of some components

---
+++ tabs/vue/Tabs.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex flex-wrap">
+        <div class="w-full">
+            <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+                <li class="cursor-pointer -mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-pink-600 bg-white': openTab !== 1, 'text-white bg-pink-600': openTab === 1}">
+                        Tab1
+                    </a>
+                </li>
+                <li class="cursor-pointer-mb-px mr-2 last:mr-0 flex-auto text-center">
+                    <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-pink-600 bg-white': openTab !== 2, 'text-white bg-pink-600': openTab === 2}">
+                        Tab2
+                    </a>
+                </li>
+            </ul>
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                <div class="px-4 py-5 flex-auto">
+                    <div class="tab-content tab-space">
+                        <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                            {{m}}
+                        </div>
+                        <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            openTab: 1
+        }
+    },
+    methods: {
+        toggleTabs: function(tabNumber){
+            this.openTab = tabNumber
+        }
+    }
+}
+</script>
